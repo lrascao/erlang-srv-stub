474f8bcffcd8c659aa406ae28239cdc39970db15
Luis Rascao <luis.rascao@gmail.com>
2014-07-04 10:40:34 +0100

    fix correct use of erlang records

--- src/builtin.erl
+++ src/builtin.erl
@@ -2,6 +2,8 @@
 
 -export([builtinPing/1]).
 
+-include("builtin_pb.hrl").
+
 % result of builtinPing is simply the same string
-builtinPing({ping_in, String}) ->
-  {ping_out, String}.
+builtinPing(#ping_in{str = Str}) ->
+  #ping_out{str = Str}.
